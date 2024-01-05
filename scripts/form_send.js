9047850163b54fac240a813cef175cfbb27b3c28
albus-owl29 <winner_in_life_skoll@proton.me>
2024-01-06 02:48:51 +0300

    Form styles test_1

--- scripts/form_send.js
+++ scripts/form_send.js
@@ -2,18 +2,17 @@ const TOKEN = "6778796489:AAF91fXiQDoePHQZGVh7Yvrc41yDDLStihM";
 const CHAT_ID = "-1002040528868";
 const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 document.getElementById('tg').addEventListener('submit', function(e) {
-	
-	e.preventDefault();
+    
+    e.preventDefault();
 
-	let message = `<b>Заявка с сайта</b>\n`;
-	message += `<b>Отправить: </b> ${ this.name.value }\n`;
-	message += `<b>Номер телефона: </b> ${ this.phone-number.value }`;
+    let message = `<b>Заявка с сайта</b>\n`;
+    message += `<b>Отправить: </b> ${ this.name.value }\n`;
+    message += `<b>Номер телефона: </b> ${ this.phoneNumber.value }`;
 
-	axios.post(URI_API, {
+    axios.post(URI_API, {
 
-		chat_id: CHAT_ID,
-		parse_mode: 'html',
-		text: message
-	})
-
-})
+        chat_id: CHAT_ID,
+        parse_mode: 'html',
+        text: message
+    })
+    })
