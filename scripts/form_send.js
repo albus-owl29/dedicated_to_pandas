bf665e697bf568cf70fbe3bb4f2e42baa3cb9e0d
albus-owl29 <winner_in_life_skoll@proton.me>
2024-01-18 04:14:30 +0300

    Fixed nav click and moded btn-primary

--- scripts/form_send.js
+++ scripts/form_send.js
@@ -1,6 +1,7 @@
 const TOKEN = "6778796489:AAF91fXiQDoePHQZGVh7Yvrc41yDDLStihM";
 const CHAT_ID = "-1002040528868";
 const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+const button = document.getElementById('btn_1');
 document.getElementById('tg').addEventListener('submit', function(e) {
     
     e.preventDefault();
@@ -15,4 +16,13 @@ document.getElementById('tg').addEventListener('submit', function(e) {
         parse_mode: 'html',
         text: message
     })
-    })
+
+    button.textContent = 'Отправлено';
+})
+
+function renameBtn() {
+
+    button.textContent = 'Отправить';
+  }
+  
+  setInterval(() => renameBtn(), 5000);
--- scripts/scroll_fix.js
+++ scripts/scroll_fix.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $('nav a').click(function(){
         
-        var otstupTop=50;
+        var otstupTop=75;
         $('body,html').animate({
         
             scrollTop: $($(this).attr('href')).offset().top-otstupTop
