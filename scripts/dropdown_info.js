95760b7aaac914f5d7407bd4bda955709a5e94a0
albus-owl29 <winner_in_life_skoll@proton.me>
2024-01-28 07:31:45 +0300

    fix dd js

--- scripts/dropdown_info.js
+++ scripts/dropdown_info.js
@@ -1,52 +1,59 @@
 jQuery(function($) {
-    $('#dd_1').click(function() {
+    var width = $(window).width();
+    if (width <= 480) {
+        $('#dd_1').click(function() {
   
-        if ($(this).parent().find('#dd_info_1').length) {
-            $('#dd_info_2, #dd_info_3, #dd_info_4').hide();
-            $(this).parent().find('#dd_info_1').slideToggle(200); 
+            if ($(this).parent().find('#dd_info_1').length) {
+                $('#dd_info_2, #dd_info_3, #dd_info_4').hide();
+                $(this).parent().find('#dd_info_1').slideToggle(500, "swing"); 
   
-            return false;
-        }
-  
-    });
+                return false;
+            }
+        });
+    }
 });
 
 jQuery(function($) {
-    $('#dd_2').click(function() {
-        if ($(this).parent().find('#dd_info_2').length) {
-            $('#dd_info_1, #dd_info_3, #dd_info_4').hide();
-            $(this).parent().find('#dd_info_2').slideToggle(200); 
+    var width = $(window).width();
+    if (width <= 480) {
+        $('#dd_2').click(function() {
+        
+            if ($(this).parent().find('#dd_info_2').length) {
+                $('#dd_info_1, #dd_info_3, #dd_info_4').hide();
+                $(this).parent().find('#dd_info_2').slideToggle(500, "swing"); 
   
-            return false;
-        }
-  
-    });
+                return false;
+            }
+        });
+    }
 });
 
 jQuery(function($) {
-    $('#dd_3').click(function() {
+    var width = $(window).width();
+    if (width <= 480) {
+        $('#dd_3').click(function() {
   
-        if ($(this).parent().find('#dd_info_3').length) {
-            $('#dd_info_1, #dd_info_2, #dd_info_4').hide();
-            $(this).parent().find('#dd_info_3').slideToggle(200); 
+            if ($(this).parent().find('#dd_info_3').length) {
+                $('#dd_info_1, #dd_info_2, #dd_info_4').hide();
+                $(this).parent().find('#dd_info_3').slideToggle(500, "swing"); 
   
-            return false;
-        }
-  
-    });
+                return false;
+            }
+        });
+    }
 });
 
 jQuery(function($) {
-    $('#dd_4').click(function() {
+    var width = $(window).width();
+    if (width <= 480) {
+        $('#dd_4').click(function() {
   
-        if ($(this).parent().find('#dd_info_4').length) {
-            $('#dd_info_1, #dd_info_2, #dd_info_3').hide();
-            $(this).parent().find('#dd_info_4').slideToggle(200); 
+            if ($(this).parent().find('#dd_info_4').length) {
+                $('#dd_info_1, #dd_info_2, #dd_info_3').hide();
+                $(this).parent().find('#dd_info_4').slideToggle(500, "swing"); 
   
-            return false;
-        }
-  
-    });
+                return false;
+            }
+        });
+    }
 });
-
-$(document).mouseup( function(e){$('.dropdown_info').hide()})
